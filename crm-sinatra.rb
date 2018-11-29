b1d182c46a5f1045037bad0edabab3701c626c5c
msynko <marina.synko@gmail.com>
2018-11-29 16:10:01 -0500

    commit

--- crm-sinatra.rb
+++ crm-sinatra.rb
@@ -2,12 +2,17 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+  @title = "About"
   erb :index
 end
 
 get '/contacts' do
-   @contacts = Contact.all
+  @contacts = Contact.all
   erb :contacts
+end
+
+ get '/about' do
+  redirect to ('/')
 end
 
 after do
